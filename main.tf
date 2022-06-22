31007bdbce90351932db50b6fc0c8e72fedd6ff9
AdaSpinal <60679768+AdaSpinal@users.noreply.github.com>
2022-06-21 23:39:15 -0600

    Add files via upload

--- main.tf
+++ main.tf
@@ -14,18 +14,36 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-ami     = "ami-0022f774911c1d690"
-instance_type = "t2.micro"
-key_name = "Llave-Ada"
-security_groups = [aws_security_group.allow_all.name]
+resource "aws_launch_template" "SM" {
+  name = "LT-Supermario-Ada"
+  description = "Template for Super Mario Game"
+  image_id = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+  key_name = "Llave-Ada"
 
- tags = {
-   Name = "Super_Mario_2022"
- }
+  vpc_security_group_ids = ["sg-04beabb75f4b4d46b"]
 
-
+    tags = {
+      Name = "Super_Mario_2022"
+    }
 }
 
+resource "aws_autoscaling_group" "bar" {
+  name= "AG-SuperMario-Ada"
+  availability_zones = ["us-east-1a"]
+  desired_capacity = 1
+  max_size = 4
+  min_size = 1 
+  
+  launch_template {
+    id = aws_launch_template.SM.id
+    version = "$Latest"
+  }
 
+  tag {
+    key = "Name"
+    value = "Proyecto-Ada"
+    propagate_at_launch = true
+  }
+}
 

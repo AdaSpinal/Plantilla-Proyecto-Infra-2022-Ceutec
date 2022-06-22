bfe78c4841ed6916c87fcf501495e5e15554225b
AdaSpinal <60679768+AdaSpinal@users.noreply.github.com>
2022-06-22 01:41:27 -0600

    Add files via upload

--- load_balance.tf
+++ load_balance.tf
@@ -7,6 +7,14 @@ resource "aws_internet_gateway" "gateway" {
 
 }
 
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "subnet" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_alb_target_group" "group" {
   name     = "SuperMarioTargetGroup"
   port     = 80
@@ -17,8 +25,45 @@ resource "aws_alb_target_group" "group" {
   }
  
   health_check {
-    path = "/login"
-    port = 80
+    path = "/"
+    port = 5000
+    interval = 10
+    timeout = 5
+    healthy_threshold = 5
+    unhealthy_threshold = 2
   }
+
+  target_type = "instance"
+
 }
 
+#Instance Attachment
+resource "aws_autoscaling_attachment" "svc_asg_external2" {
+  alb_target_group_arn = "${aws_alb_target_group.group.arn}"
+  autoscaling_group_name = "${aws_autoscaling_group.bar.id}" 
+  
+}
+
+resource "aws_alb" "application-alb" {
+    name = "LB-ProyectoSuperMario"
+    internal = false
+    ip_address_type = "ipv4"
+    load_balancer_type ="application"
+    security_groups = ["sg-028e0d5f7222fc799"]
+    subnets = data.aws_subnet_ids.subnet.ids 
+
+        tags = {
+          Name = "whiz-alb"
+        }
+
+}
+
+resource "aws_alb_listener" "alb-listener" {
+  load_balancer_arn  = aws_alb.application-alb.arn
+  port = 80
+  protocol = "HTTP"
+    default_action {
+        target_group_arn = "${aws_alb_target_group.group.arn}"
+        type = "forward"
+    }
+}
